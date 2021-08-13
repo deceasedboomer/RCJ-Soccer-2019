98e95c1b7a1c8dc4f185d58eb4e829e6b28b355a
Deng Jun <81004403+deceasedboomer@users.noreply.github.com>
2021-08-13 18:37:46 +0800

    Update Camera.ino

--- Camera.ino
+++ Camera.ino
@@ -1,6 +1,6 @@
 // ***DISCLAIMER, my camera program never actually worked*** //
 
-//attempted to use serial communication with an OPENMV M7 program initially which we eventually abandoned due to a lack of time.
+//attempted to use serial communication with an OPENMV M7 camera initially which we eventually abandoned due to a lack of time.
 
 void readcam()  {
   while(Serial2.available()>0)  Serial.println(Serial2.read());
